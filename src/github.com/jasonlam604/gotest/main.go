bfc3faa88d645591c7b57a774329754664a6c3ca
Jason Lam <jasonlam604@gmail.com>
2016-12-14 21:52:17 -0800

    Init commit

---
+++ src/github.com/jasonlam604/gotest/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/example/stringutil"
+)
+
+func main() {
+	fmt.Println(reverseMe("hello"))
+}
+
+func reverseMe(name string) string {
+    	return stringutil.Reverse(name)
+}
---
+++ src/github.com/jasonlam604/gotest/main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestReverseMe(t *testing.T) {
+	assert.Equal(t, "olleh",reverseMe("hello"))
+}
+
+func TestReverseMeAgain(t *testing.T) {
+	assert.Equal(t, "hello",reverseMe("olleh"))
+}
