e975d92ea97b6cb3911a940b7ed6234a93e7b8ff
Jason Lam <jasonlam604@gmail.com>
2016-12-31 11:33:34 -0800

    Add "main" test

--- src/github.com/jasonlam604/gotest/main_test.go
+++ src/github.com/jasonlam604/gotest/main_test.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"testing"
+	"flag"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
+
+var systemTest *bool
+
+func init() {
+	systemTest = flag.Bool("systemTest", true, "Set to true when running system tests")
+}
+
+func TestSystem(t *testing.T) {
+	if *systemTest {
+		main()
+	}
+}
 
 func TestReverseMe(t *testing.T) {
 	assert.Equal(t, "olleh", reverseMe("hello"))
